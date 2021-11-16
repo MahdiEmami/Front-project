b9c1e6c3647a0256dd3861791f5e9013d5e73b3a
MahdiEmami <92790844+MahdiEmami@users.noreply.github.com>
2021-11-16 04:18:29 +0330

    Delete reg.php

--- reg.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn=new mysqli('localhost','root','','users');
-$sql = "INSERT INTO names (userName, email, psword)
-VALUES ($_POST[userName],$_POST[email],$_POST[psword])";
-$conn->close();
-?>
