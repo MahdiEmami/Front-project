e1d09313ac1c7618b4d098eb1bfb1c968411a60e
MahdiEmami <92790844+MahdiEmami@users.noreply.github.com>
2022-01-06 09:07:03 +0330

    Delete Front-project directory

--- Front-project/js/main.js
+++
@@ -1,8 +0,0 @@
-//hamburger handler start
-const bars= document.querySelector('.header__bar');
-let isopen=document.querySelector('.header__links');
-bars.addEventListener('click',()=>{
-isopen.classList.toggle('header_close');
-}
-)
-//hamburger handler end
